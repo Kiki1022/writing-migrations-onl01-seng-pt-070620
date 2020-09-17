14638b5fbbe681dbdf1dc39f2e6e7208137e371e
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-17 00:49:12 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,10 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   
   def change
     create_table :students do |t|
-    t.string :name
-    # t.string :genre
-    # t.integer :age
-    # t.string :hometown
-  end
+      t.string :name
+    end
   end
 end
